e791534aa6e3790e345531c5a2c328a90245ba90
Madjac33 <bavosam1@tcnj.edu>
2025-02-23 00:20:17 -0500

    switch scenes

---
+++ Comfort Food/Assets/FoodController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public GameObject[] ingredients;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Comfort Food/Assets/GameController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject book;
+    public GameObject kitchen;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartCooking() {
+        book.transform.Translate(0, 1000, 0);
+        kitchen.transform.Translate(0, 1000, 0);
+    }
+}
